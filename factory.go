e769dbaf9c17c83f26d0e0602c916df3ee083c10
Nicolas Boulay <nicolas@boulay.name>
2020-06-01 11:22:29 +0200

    cleaning

--- factory.go
+++ factory.go
@@ -5,67 +5,12 @@ import (
 	"fmt"
 )
 
-//type nodeFactory interface {
-//	New(interface{}) node
-//}
-//
-//type ORFactory struct {
-//}
-//
-//func (*ORFactory) New(in interface{}) node {
-//	return &OR{&StrZero}
-//}
-//
-//type CoherentFactory struct {
-//}
-//
-//func (*CoherentFactory) New(in interface{}) node {
-//	return &Coherent{node{}}
-//}
-//
-//type LeafFactory struct {
-//}
-//
-//func (*LeafFactory) New(in interface{}) node {
-//	return &leaf{}
-//}
-
-//func Choose(key interface{}) nodeFactory {
-//	v := reflect.ValueOf(key)
-//	
-//	switch v.Kind() {
-////	case reflect.Bool:
-////		fmt.Printf("bool: %v\n", v.Bool())
-////	case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64:
-////		fmt.Printf("int: %v\n", v.Int())
-////	case reflect.Uint, reflect.Uint8, reflect.Uint32, reflect.Uint64:
-////		fmt.Printf("int: %v\n", v.Uint())
-////	case reflect.Float32, reflect.Float64:
-////		fmt.Printf("float: %v\n", v.Float())
-//	case reflect.String:
-//		if v.String() == "OR" {
-//			return &ORFactory{}
-//		}
-////	case reflect.Slice:
-////		fmt.Printf("slice: len=%d, %v\n", v.Len(), v.Interface())
-////	case reflect.Map:
-////		fmt.Printf("map: \n");
-////		iter := reflect.ValueOf(yaml).MapRange()
-////		for iter.Next() {
-////			k := iter.Key()
-////			fmt.Printf("[%v] ", k);
-////			v := iter.Value()
-////			yamlToNode(v.Interface())
-////		}
-////	case reflect.Chan:
-////		fmt.Printf("chan %v\n", v.Interface())
-//	default:
-//		fmt.Printf("\n%v\n",v)
-//	}
-//	return nil
-//}
-
-//map et struct sont gérer de la même façon, elle gène un array pour gérer les clefs particulières comme "OR" directement
+// le pattern d'abstract factory n'aide pas vraiment ici, car C'est vraiment le contenu exact de l'entré qui compte.
+// Je n'ai pas trouvé de moyen d'utiliser les interfaces pour éviter ça. Le gros switch est inévitable,
+// pour créer les nodes.
+// 
+//map et struct sont gérer de la même façon et doivent l'être, elle gène un array pour gérer les clefs particulières
+// comme "OR" directement
 //Kind() pas encore utilisé : 
         //Uintptr
 	//Complex64
@@ -105,20 +50,6 @@ func  BigUglySwitch(in interface{}) node {
 			v := iter.Value()
 			if k.Kind() == reflect.String {
 				returnArrayNode = appendArrayWithKey(returnArrayNode, mapNode, k.String(), v.Interface())
-				//valueNode := BigUglySwitch(v.Interface()) 
-				//switch k.String() {
-				//case "OR":
-				//	returnArrayNode = append(returnArrayNode,&OR{valueNode})
-				//case "Coherent":
-				//	returnArrayNode = append(returnArrayNode,&Coherent{valueNode})
-				//case "Not":
-				//	returnArrayNode = append(returnArrayNode,&Not{valueNode})
-				//default:
-				//	{
-				//		keyNode := BigUglySwitch(k.Interface())
-				//		mapNode[keyNode] = valueNode
-				//	}
-				//}
 			}			
 		}
 		if len (mapNode) != 0 {
